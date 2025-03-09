c9b3f2c942056bf31278943a4cc3140b7678aedb
ZKwitt <thepersonmrt@gmail.com>
2025-03-09 14:22:51 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from openai import OpenAI
- 
-base_url = "<https://api.aimlapi.com/v1>"
- api_key = "d0d50f3916464355a79388520278a67c"
- system_prompt = "You are a travel agent. Be descriptive and helpful."
- user_prompt = "Tell me about San Francisco"
- 
+
+base_url = "https://api.aimlapi.com/v1"
+api_key = "d0d50f3916464355a79388520278a67c"
+system_prompt = "You are a travel agent. Be descriptive and helpful."
+user_prompt = "Tell me about San Francisco"
+
 api = OpenAI(api_key=api_key, base_url=base_url)
- 
+
 def main():
     completion = api.chat.completions.create(
         model="mistralai/Mistral-7B-Instruct-v0.2",
@@ -17,11 +17,11 @@ def main():
         temperature=0.7,
         max_tokens=256,
     )
- 
+
     response = completion.choices[0].message.content
- 
+
     print("User:", user_prompt)
     print("AI:", response)
- 
+
 if __name__ == "__main__":
     main()
